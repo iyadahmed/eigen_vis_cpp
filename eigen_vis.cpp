e9e25dc9d3c7bf4d2b8cf8052c9507de378effc9
Iyad Ahmed <iyadahmed430@gmail.com>
2023-10-05 02:20:34 +0300

    GUI inception with raylib

--- eigen_vis.cpp
+++ eigen_vis.cpp
@@ -1,9 +1,5 @@
 #include <chrono>
 #include <complex>
-#include <cstddef> // for size_t
-#include <cstdint>
-#include <cstring> // for memset
-#include <fstream>
 #include <iostream>
 #include <random>
 
@@ -15,108 +11,134 @@
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
 
+#include <raylib.h>
+
 using namespace std::complex_literals;
 
-constexpr size_t N = 4096 * 2;
+constexpr int WINDOW_WIDTH = 1280;
+constexpr int WINDOW_HEIGHT = 800;
+constexpr int NUM_SAMPLES = 1e4;
+
+constexpr int TARGET_FPS = 60;
+
+constexpr int FRAME_TIME_POS_X = 10;
+constexpr int FRAME_TIME_POS_Y = 10;
+constexpr int FRAME_TIME_FONT_SIZE = 20;
+
+constexpr float VIEWPORT_RANGE_START = -5;
+constexpr float VIEWPORT_RANGE_END = 5;
 
 int main()
 {
-    // Note from docs: "With Eigen 3.3, and a fully C++11 compliant compiler calling initParallel() is optional."
+    // Note from Eigen's docs: "With Eigen 3.3, and a fully C++11 compliant compiler calling initParallel() is optional."
     Eigen::initParallel();
 
-    Eigen::Matrix<uint8_t, -1, -1> output(N, N);
-    output.fill(255);
     std::random_device rng_device;
     std::default_random_engine rng_engine(rng_device());
     std::uniform_real_distribution<float> uniform_dist(-10, 10);
 
-    constexpr int num_samples = 1e6;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Eigen Visualizer");
 
-    auto t0 = std::chrono::high_resolution_clock::now();
-#pragma omp parallel for
-    for (int i = 0; i < num_samples; i++)
+    Image image = GenImageColor(WINDOW_WIDTH, WINDOW_HEIGHT, RAYWHITE);
+    Texture2D texture = LoadTextureFromImage(image);
+
+    SetTargetFPS(TARGET_FPS);
+    while (!WindowShouldClose())
     {
-        Eigen::ComplexEigenSolver<Eigen::MatrixXcd> ces;
-        float t1 = uniform_dist(rng_engine);
-        float t2 = uniform_dist(rng_engine);
-
-        Eigen::MatrixXcd A(7, 7);
-        A(0, 0) = -1i;
-        A(0, 1) = -1i;
-        A(0, 2) = 1i;
-        A(0, 3) = 0;
-        A(0, 4) = -1i;
-        A(0, 5) = 0;
-        A(0, 6) = -1i;
-
-        A(1, 0) = -1i;
-        A(1, 1) = 0;
-        A(1, 2) = 0.3;
-        A(1, 3) = -1i;
-        A(1, 4) = 0;
-        A(1, 5) = 0;
-        A(1, 6) = -1i;
-
-        A(2, 0) = 0;
-        A(2, 1) = -1i;
-        A(2, 2) = 0;
-        A(2, 3) = 0.3;
-        A(2, 4) = 1i;
-        A(2, 5) = 0.3;
-        A(2, 6) = 1i;
-
-        A(3, 0) = 0;
-        A(3, 1) = -1i;
-        A(3, 2) = 1i;
-        A(3, 3) = -1i;
-        A(3, 4) = 1i;
-        A(3, 5) = 0.3;
-        A(3, 6) = -1i;
-
-        A(4, 0) = 0;
-        A(4, 1) = 1i;
-        A(4, 2) = 1i;
-        A(4, 3) = 0;
-        A(4, 4) = 0;
-        A(4, 5) = -1i;
-        A(4, 6) = 1i;
-
-        A(5, 0) = 0;
-        A(5, 1) = 0.3;
-        A(5, 2) = 0;
-        A(5, 3) = -1i;
-        A(5, 4) = 0;
-        A(5, 5) = t1;
-        A(5, 6) = 0;
-
-        A(6, 0) = -1i;
-        A(6, 1) = t2;
-        A(6, 2) = 1i;
-        A(6, 3) = 1i;
-        A(6, 4) = 1i;
-        A(6, 5) = 0.3;
-        A(6, 6) = -1i;
-
-        ces.compute(A, false);
-        for (const auto &v : ces.eigenvalues())
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        ImageClearBackground(&image, RAYWHITE);
+        auto t0 = std::chrono::high_resolution_clock::now();
+#pragma omp parallel for
+        for (int i = 0; i < NUM_SAMPLES; i++)
         {
-            double x = v.real();
-            double y = v.imag();
-            auto xi = static_cast<size_t>(((x + 5) / 10) * N);
-            auto yi = static_cast<size_t>(((y + 5) / 10) * N);
-            if (xi < N && yi < N)
-                output(xi, yi) = 0;
-        }
-    }
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << std::endl;
+            Eigen::ComplexEigenSolver<Eigen::MatrixXcd> ces;
+            float t1 = uniform_dist(rng_engine);
+            float t2 = uniform_dist(rng_engine);
 
-    std::ofstream ofs("output.pgm", std::ofstream::binary);
-    ofs << "P5\n"
-        << N << ' ' << N << "\n255\n";
-    // Otherwise the image will be flipped due to how PGM file format is layed out
-    output.rowwise().reverseInPlace();
-    ofs.write(reinterpret_cast<char *>(output.data()), N * N * sizeof(uint8_t));
-    std::cout << "Finished" << std::endl;
+            Eigen::MatrixXcd A(7, 7);
+            A(0, 0) = -1i;
+            A(0, 1) = -1i;
+            A(0, 2) = 1i;
+            A(0, 3) = 0;
+            A(0, 4) = -1i;
+            A(0, 5) = 0;
+            A(0, 6) = -1i;
+
+            A(1, 0) = -1i;
+            A(1, 1) = 0;
+            A(1, 2) = 0.3;
+            A(1, 3) = -1i;
+            A(1, 4) = 0;
+            A(1, 5) = 0;
+            A(1, 6) = -1i;
+
+            A(2, 0) = 0;
+            A(2, 1) = -1i;
+            A(2, 2) = 0;
+            A(2, 3) = 0.3;
+            A(2, 4) = 1i;
+            A(2, 5) = 0.3;
+            A(2, 6) = 1i;
+
+            A(3, 0) = 0;
+            A(3, 1) = -1i;
+            A(3, 2) = 1i;
+            A(3, 3) = -1i;
+            A(3, 4) = 1i;
+            A(3, 5) = 0.3;
+            A(3, 6) = -1i;
+
+            A(4, 0) = 0;
+            A(4, 1) = 1i;
+            A(4, 2) = 1i;
+            A(4, 3) = 0;
+            A(4, 4) = 0;
+            A(4, 5) = -1i;
+            A(4, 6) = 1i;
+
+            A(5, 0) = 0;
+            A(5, 1) = 0.3;
+            A(5, 2) = 0;
+            A(5, 3) = -1i;
+            A(5, 4) = 0;
+            A(5, 5) = t1;
+            A(5, 6) = 0;
+
+            A(6, 0) = -1i;
+            A(6, 1) = t2;
+            A(6, 2) = 1i;
+            A(6, 3) = 1i;
+            A(6, 4) = 1i;
+            A(6, 5) = 0.3;
+            A(6, 6) = -1i;
+
+            ces.compute(A, false);
+            for (const auto &v : ces.eigenvalues())
+            {
+                double x = v.real();
+                double y = v.imag();
+                auto xi = static_cast<size_t>(((x - VIEWPORT_RANGE_START) / (VIEWPORT_RANGE_END - VIEWPORT_RANGE_START)) * WINDOW_WIDTH);
+                auto yi = static_cast<size_t>(((y - VIEWPORT_RANGE_START) / (VIEWPORT_RANGE_END - VIEWPORT_RANGE_START)) * WINDOW_HEIGHT);
+                if (xi < WINDOW_WIDTH && yi < WINDOW_HEIGHT)
+                {
+                    ImageDrawPixel(&image, xi, WINDOW_HEIGHT - 1 - yi, BLACK);
+                }
+            }
+        }
+        UpdateTexture(texture, image.data);
+        DrawTexture(texture, 0, 0, WHITE);
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto frame_time = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+        DrawText(("Frame time: " + std::to_string(frame_time)).c_str(),
+                 FRAME_TIME_POS_X, FRAME_TIME_POS_Y, FRAME_TIME_FONT_SIZE, BLUE);
+
+        EndDrawing();
+    }
+    UnloadTexture(texture);
+    CloseWindow();
+
+    ExportImage(image, "eigen_vis.png");
     return 0;
 }
---
+++ extern/raylib-4.5.0/README.iyad.md
@@ -0,0 +1,6 @@
+Original Raylib 4.5.0 Source Code downloaded from https://github.com/raysan5/raylib/releases/tag/4.5.0
+Stripped off everything except `./LICENSE` and `./src/` and everything needed for CMake to work.
+Modifications: increase minimum CMake version to 3.16
+
+- iyad
+Kudos: https://github.com/tsoding/musializer/tree/master/raylib/raylib-4.5.0
